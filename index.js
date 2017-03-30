077bbe5838533d24d471f97d8e0d950ce143f72e
Kaushik Nagaraj <kaushikstan@gmail.com>
2017-03-29 20:06:26 -0400

    post

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/', (request, response) => response.send("POST"))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
