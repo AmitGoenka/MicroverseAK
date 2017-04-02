da3b04431fec34f8dccc9366eb9ec998a5fb61b4
Amit Goenka <master.goenka@gmail.com>
2017-04-02 11:04:01 -0500

    Post api for creating events

--- events.js
+++ events.js
@@ -8,17 +8,27 @@ function Event(id, title, desc, date) {
   this.date = date;
 }
 
-let eventInstance = new Event();
-
 let arr = [];
 
-for(let i=0; i<10; i++) {
-  arr.push(new Event(i, `title `+i, 'desc '+i, 'date '+i));
+for(let i = 0; i < 10; i++) {
+  arr.push(new Event(i, `title ` + i, 'desc ' + i, 'date ' + i));
 }
 
 router.get('/', (request, response, next) => {
-  console.log("here")
   response.send(arr);
 })
+
+router.get('/:id', (request, response, next) => {
+  const id = parseInt(request.params.id);
+  response.send(arr.filter(x => {
+    return x.id === id;
+  }));
+})
+
+router.post('/', (request, response, next) => {
+  newEvent = new Event(request.body.id, request.body.title, request.body.description, request.body.date);
+  arr.push(newEvent);
+  response.send(arr[arr.length - 1]);
+});
 
 module.exports = router;
