0309393d7e0280b576618ec22ebf1d2e246fa558
Kaushik Nagaraj <kaushikstan@gmail.com>
2017-05-21 11:02:28 -0400

    refactored connect

--- db/index.js
+++ db/index.js
@@ -17,15 +17,20 @@ const assert = require('assert');
 
 const dbUrl = 'mongodb://localhost:27017/microverse';
 
-var insert = function(data) {
+var con = (action) => {
   MongoClient.connect(dbUrl, function(err, db) {
-  assert.equal(null, err);
-  console.log('Connected succesfully to database');
+    assert.equal(null, err);
+    console.log('Connected succesfully to database');
+    action();
+  });
+}
 
-  model.insertDocuments(db, function() {
-    db.close();
-  }, data)
-});
+var insert = function(data) {
+  con(() => {
+    model.insertDocuments(db, function() {
+      db.close();
+    }, data);
+  })
 }
 
 module.exports = {
--- db/model-driver.js
+++ db/model-driver.js
@@ -21,21 +21,21 @@ const insertDocuments = function(db, callback, data) {
 //   { "title": "title 5", "description": "desc 5", "date": "date 5" }
 // ]
 
-const updateDocuments = function(db, callback) {
+const updateDocuments = function(db, callback, data) {
   const events = db.collection('events');
-  events.insertMany([
-    { "title": "title 1", "description": "desc 1", "date": "date 1" },
-    { "title": "title 2", "description": "desc 2", "date": "date 2" },
-    { "title": "title 3", "description": "desc 3", "date": "date 3" },
-    { "title": "title 4", "description": "desc 4", "date": "date 4" },
-    { "title": "title 5", "description": "desc 5", "date": "date 5" }
-  ],
+  events.updateOne({
+    "_id": `Object_id(${data.id})`
+  },{
+    $set: {
+      "title": data.title
+    }
+  },
   function(err, result) {
     assert.equal(err, null);
     console.log(result);
-    assert.equal(5, result.result.n);
-    assert.equal(5, result.ops.length);
-    console.log('Inserted 5 events successfully');
+    assert.equal(data.length, result.result.n);
+    assert.equal(data.length, result.ops.length);
+    console.log('Updated events successfully');
     callback(result);
   });
 }
