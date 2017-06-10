e08f3b04e10979747403f6325976b78fbbd64d6e
Amit Goenka <amit.goenka@matrixcare.com>
2017-06-10 11:08:03 -0500

    testing with pasport http

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+// const LocalStrategy = require('passport-local').Strategy;
 // const session = require('express-session');
 // const flash = require('connect-flash');
 const BasicStrategy = require('passport-http').BasicStrategy;
@@ -43,7 +43,6 @@ module.exports = app;
 // }))
 
 function findUser(credentials, action) {
-  console.log(credentials);
   if(!credentials) return action("No credentials provided");
   else if(credentials.username !== "Amit") return action(null, false, {message: 'Incorrect Username'});
   else if(credentials.password !== "micro") return action(null, false, {message: 'Incorrect Password'});
@@ -52,36 +51,44 @@ function findUser(credentials, action) {
 
 // ---------------------------------------------------------
 
-app.get('/login', (req, res, next) => {
-  // res.render('local', {message: req.flash('some error we dont know')});
-  // res.send(req.flash('some error we dont know'));
-  res.send('some error we dont know');
+// app.get('/login', (req, res, next) => {
+//   // res.render('local', {message: req.flash('some error we dont know')});
+//   // res.send(req.flash('some error we dont know'));
+//   res.send('some error we dont know');
+// });
+
+// Passport-HTTP
+const strategy = new BasicStrategy((username, password, done) => {
+  findUser({username: username, password: password}, (err, user, message) => {
+    if(err) return done(err);
+    if(!user) {
+      console.log(message);
+      return done(null, false, message);
+    }
+    return done(null, user);
+  })
 });
+
+passport.use(strategy);
 
 // app.post('/login', passport.authenticate("local", { successRedirect: '/', failureRedirect: '/login', failureFlash: false, session: false}));
 app.post('/login', (req, res, next) => {
-  passport.authenticate("basic", (err, user, info) => {
-        console.log(err, user, info);
+  passport.authenticate("basic", {session: false}, (err, user, info, x, y, z) => {
+    console.log(err, user, info, x, y, z);
     if(err) return next(err);
-    if(!user) res.status(403).send(info);
-    res.send(user);
+    if(!user) {
+      res.status(403).send(info);
+    } else {
+      res.send(user);
+    }
   })(req, res, next);
 });
 
 // ---------------------------------------------------------
 
-// Passport-HTTP
-passport.use(new BasicStrategy((username, password, done) => {
-  console.log("basicstrategy")
-  findUser({username: username, password: password}, (err, user, message) => {
-    if(err) return done(err);
-    if(!user) return done(null, false, message);
-    return done(null, user);
-  })
-}))
+
 
 // app.post('/login', passport.authenticate("basic", {session: false}), (err, user, info) => {
-//     console.log(err, user, info);
 //     if(err) return next(err);
 //     if(!user) res.status(403).send(info);
 //     res.send(user);
